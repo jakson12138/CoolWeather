e2722015dd1e9b7892823743f3f5d2c7c61396ef
wuzesheng <wuzesheng9@gmail.com>
2019-07-30 14:27:07 +0800

    加入数据库和表

---
+++ app/src/main/java/com/example/coolweather/db/Country.java
@@ -0,0 +1,48 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Country extends DataSupport {
+
+    private int id;
+
+    private String countryName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountyName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+}
+
